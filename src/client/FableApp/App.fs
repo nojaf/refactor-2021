616472ab4bf37b278cb87f3311b642d7a41ef9a3
Florian Verdonck <florian.verdonck@outlook.com>
2021-03-06 08:14:55 +0000

    Scaffold snowpack project.

---
+++ src/client/FableApp/App.fs
@@ -0,0 +1,13 @@
+﻿module FableApp.App
+
+open Feliz
+open Fable.Core.JsInterop
+open Fable.React
+
+[<ReactComponent>]
+let private App () =
+    div [] [
+        str "App"
+    ]
+
+exportDefault App
