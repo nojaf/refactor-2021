a577004c282b7575d1a9612c21c4527805849682
Florian Verdonck <florian.verdonck@outlook.com>
2021-03-13 21:19:05 +0000

    Set Docker buildkit before release build.

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-docker build -f Release.Dockerfile --output type=local,dest=./artifacts .
+env DOCKER_BUILDKIT=1 docker build -f Release.Dockerfile --output type=local,dest=./artifacts .
 docker run -it --rm -v "$(pwd)/artifacts:/app" -w "/app" mcr.microsoft.com/azure-cli bash azure.sh
