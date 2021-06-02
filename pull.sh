7980a6fae66c955580c39c8afbe71dd99cbdb4c1
Florian Verdonck <florian.verdonck@outlook.com>
2021-06-02 13:08:32 +0000

    Add azure function node runtime

--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker pull mcr.microsoft.com/dotnet/sdk:5.0-focal
 docker pull node:14.16
+docker pull mcr.microsoft.com/azure-functions/node:3.0.3233-node12-core-tools
 docker pull mcr.microsoft.com/azure-functions/dotnet:3.0.3284-dotnet3-core-tools
 docker pull node:15.11
 docker pull mcr.microsoft.com/powershell:alpine-3.12
